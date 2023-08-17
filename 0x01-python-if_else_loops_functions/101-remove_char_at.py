660d509d0d0376df57b30b1441fe45cc615801ca
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-08-17 07:49:15 +0000

    d

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):  # Handle out of ra
+        return str
+
+    new_str = ""
+    for idx, char in enumerate(str):
+        if idx != n:
+            new_str += char
+            
+    return new_str
