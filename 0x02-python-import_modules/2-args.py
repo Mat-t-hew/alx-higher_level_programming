ed5108a496712653748c2a9285c62d2e1a223209
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-08-24 09:59:55 +0000

    02

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def main()
+    arg_count = len(sys.argv) - 1
+
+    if arg_count == 0:
+        print("Number of arguments:.")
+    elif arg_count == 1:
+        print("Number of argument:")
+    else:
+        print("Number of arguments:")
+
+    for i, arg in enumerate(sys.argv[1:], 1):
+        print("{}: {}".format(i, arg))
+
+if __name__ == "__main__":
+    main()
+
