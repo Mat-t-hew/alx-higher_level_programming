fa733e2995a02f3eec509b4e22875d6debbde319
Mat-t-hew <n.m.ikenna2@gmail.com>
2024-01-14 21:09:20 +0200

    errors

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const sum = (function add (a, b) {
-    return parseInt(a) + parseInt(b);
-  })(process.argv[2], process.argv[3]);
-  console.log(sum);
-  
+
+const sum = (function add(a, b) {
+  return parseInt(a) + parseInt(b);
+})(process.argv[2], process.argv[3]);
+
+console.log(sum);
