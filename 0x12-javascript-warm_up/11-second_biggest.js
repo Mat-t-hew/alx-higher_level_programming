50e86c730a9688d7f470cd2c09a1d7f0aa9fa2ae
Mat-t-hew <n.m.ikenna2@gmail.com>
2024-01-14 21:23:39 +0200

    errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const argsList = process.argv.slice(2).map(num => parseInt(num));
 const sortedList = argsList.slice().sort((a, b) => a - b);
 
-if (sortedList.length <= 1) {
+if (sortedList.length < 2) {
   console.log(0);
 } else {
   console.log(sortedList[1]);
