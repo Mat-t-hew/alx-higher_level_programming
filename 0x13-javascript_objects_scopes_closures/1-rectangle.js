0de84412ecdcddc4df3bf41c22fa91caf1dc0699
Mat-t-hew <n.m.ikenna2@gmail.com>
2024-01-20 17:44:23 +0200

    errors

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -8,8 +8,8 @@
  */
 
 module.exports = class Rectangle {
-    constructor (width, height) {
-      this.width = width;
-      this.height = height;
-    }
-  };
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+}
+};
