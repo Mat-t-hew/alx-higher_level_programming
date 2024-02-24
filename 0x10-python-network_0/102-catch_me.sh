f83dee97dcc45152359f641cd69cb194f2291c61
Mat-t-hew <n.m.ikenna2@gmail.com>
2024-02-24 21:00:16 +0200

    exe

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# curl url till we get "You got me!"
-curl -sL http://0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: You find me!"
+
+# Make a POST request to the server
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
