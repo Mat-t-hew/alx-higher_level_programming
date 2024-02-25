3fe7cf11f01f879433ede3b4c0f0a7978b22a788
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-02-25 11:39:12 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,23 +4,23 @@ const epId = process.argv[2];
 
 // Check if movie ID argument is provided
 if (!epId) {
-    console.log('Usage: ./script.js <movie_id>');
-    process.exit(1);
+  console.log('Usage: ./script.js <movie_id>');
+  process.exit(1);
 }
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${epId}`;
 
 request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-        process.exit(1);
-    }
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
 
-    if (response.statusCode !== 200) {
-        console.error('Error:', response.statusMessage);
-        process.exit(1);
-    }
+  if (response.statusCode !== 200) {
+    console.error('Error:', response.statusMessage);
+    process.exit(1);
+  }
 
-    const movie = JSON.parse(body);
-    console.log(movie.title);
+  const movie = JSON.parse(body);
+  console.log(movie.title);
 });
