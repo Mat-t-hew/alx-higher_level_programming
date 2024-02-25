d95f5214f053ad7e9b554d1845788168790f78b9
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-02-25 11:26:10 +0200

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,7 +11,7 @@ request.get(apiUrl, function (error, response, body) {
     for (let i = 0; i < films.length; i++) {
       const charList = films[i].characters;
       for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi.co/api/people/18/' || charList[j] === 'http://swapi.co/api/people/18/') {
+        if (charList[j] === 'https://swapi-api.alx-tools.com/api/films/18/' || charList[j] === 'https://swapi-api.alx-tools.com/api/films//18/') {
           countWedge += 1;
         }
       }
