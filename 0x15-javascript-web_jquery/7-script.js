cbae42a80251b410b87e5ec9561ec3dc0839392c
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-02-29 05:21:04 +0200

    Update 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function getCharacter () {
-  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function apiName (data) {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function apiName (data) {
     $('#character').text(data.name);
   });
 });
