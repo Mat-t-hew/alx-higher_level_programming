9753f2386db1e14a063379c6ced7131beec33d7d
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-02-29 05:29:19 +0200

    Create 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+<head>
+  <script>
+    document.addEventListener('DOMContentLoaded', () => {
+      const header = document.querySelector('header');
+      header.style.color = '#FF0000';
+    });
+  </script>
+</head>
